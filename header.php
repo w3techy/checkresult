3d4bc9a3f28b68ce5aec4b1f7a51db70f8c9507e
w3techy <tjosegood@gmail.com>
2019-05-06 23:02:05 +0100

    header address bar

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 	<meta name="viewport" content="width=device-width" />
+	 <meta name="theme-color" content="wheat"/>
 	<title>Result</title>
 <link href="style.css" rel="stylesheet" type="text/css"/>
 </head>
